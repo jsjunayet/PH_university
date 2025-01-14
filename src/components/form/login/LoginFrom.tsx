7284e26b63b2b6d4d9c12693c114e49a3942fa40
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-01-14 23:21:14 +0600

    successfull added

--- src/components/form/login/LoginFrom.tsx
+++ src/components/form/login/LoginFrom.tsx
@@ -1,12 +1,19 @@
 import { Button } from 'antd';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useLoginMutation } from '../../../redux/features/authApi';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks/app';
+import { setuser } from '../../../redux/features/auth/authSlice';
+import { jwtDecode } from "jwt-decode";
+
 type Inputs = {
     id: string
     password: string
   }
 const LoginForm = () => {
   const [login]=useLoginMutation()
+  const dispatch = useAppDispatch()
+  const token = useAppSelector(state=>state.auth.token)
+  console.log(token);
     const {register, handleSubmit}=useForm<Inputs>()
     const onSubmit:SubmitHandler<Inputs> = async(data)=> {
       const payload ={
@@ -14,7 +21,10 @@ const LoginForm = () => {
         password:data.password
       }
       console.log(payload);
-     const res = await login(payload)
+     const res = await login(payload).unwrap()
+     const decoded = jwtDecode(res.data.accessToken);
+     console.log(decoded);
+     dispatch(setuser({token:res.data.accessToken, user:decoded.userId}))
      console.log(res);
 
     }
