e7832db9c0ef00fd01fc875fa1907958df9338c4
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-01-16 10:05:27 +0600

    form create

--- src/components/form/login/LoginFrom.tsx
+++ src/components/form/login/LoginFrom.tsx
@@ -1,11 +1,13 @@
-import { Button } from 'antd';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Button, Row } from 'antd';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { useLoginMutation } from '../../../redux/features/authApi';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks/app';
 import { setuser } from '../../../redux/features/auth/authSlice';
 import { jwtDecode } from "jwt-decode";
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
+import PHForm from './PHForm';
+import PHInput from './PHInput';
 
 type Inputs = {
     id: string
@@ -17,15 +19,19 @@ const LoginForm = () => {
   const [login]=useLoginMutation()
   const dispatch = useAppDispatch()
   const token = useAppSelector(state=>state.auth.token)
+  const defaultValues = {
+    userId: '0001',
+    password: 'admin12345',
+  };
   console.log(token);
-    const {register, handleSubmit}=useForm<Inputs>()
-    const onSubmit:SubmitHandler<Inputs> = async(data)=> {
+  const onSubmit = async (data: FieldValues) => {
      try{
       toast.loading('Loading...', { id: toastId });
       const payload ={
-        id: data.id,
+        id: data.userId,
         password:data.password
       }
+      console.log(payload);
      const res = await login(payload).unwrap()
      const decoded = jwtDecode(res.data.accessToken);
     
@@ -38,15 +44,13 @@ const LoginForm = () => {
 
     }
     return (
-        <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-          <label>ID:</label>
-           <input type="text"  id="id" {...register("id")} /> 
-           <label >Password: </label>
-           <input type="text" id="password"{...register("password")} />
-           <Button htmlType='submit'>Login</Button>
-          </form>
-        </div>
+      <Row justify="center" align="middle" style={{ height: '100vh' }}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <PHInput type="text" name="userId" label="ID:" />
+        <PHInput type="text" name="password" label="Password" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
     );
 };
 
--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -1,7 +1,49 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "./store";
+import { logout, setuser } from "./features/auth/authSlice";
+const baseQuery =  fetchBaseQuery({
+    baseUrl: 'http://localhost:5000/api/v1', 
+    credentials: 'include', 
+    prepareHeaders: (headers, { getState }) => {
+      const state = (getState() as RootState); 
+      const token = state.auth.token 
+      if (token) {
+        headers.set('Authorization', ` ${token}`);
+      }    
+      return headers; 
+    },
+  })
+
+  const baseQueryRefreshToken =async(arg,api,extraOptions)=>{
+    let result = await baseQuery(arg,api,extraOptions)
+    console.log(result.error?.status, "result");
+    if(result.error?.status===500){
+        const refreshResponse = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+            method: 'POST',
+            credentials: 'include', // Send cookies with the request
+          });
+   if(refreshResponse.ok){
+       
+    const accessToken = await refreshResponse.json()
+    api.dispatch(
+      setuser({
+          token:accessToken.data.accessToken,
+          user:api.getState().auth.user
+      })
+    )
+    result = await baseQuery(arg,api, extraOptions)
+   }else{
+    await refreshResponse.json()
+    api.dispatch(logout())
+   }
+      
+    }
+    return result
+    
+  }
 
 export const baseApi = createApi({
     reducerPath:"authApi",
-    baseQuery: fetchBaseQuery({baseUrl:'http://localhost:5000/api/v1',credentials:"include"}),
+    baseQuery:baseQueryRefreshToken,
     endpoints:()=>({})
 })
